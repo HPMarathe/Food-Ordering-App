90aa4b748841706ce9c6ddb7446f55d3d15c42fb
Hrushi <marathehrushikesh930@gmail.com>
2023-08-20 00:58:06 +0530

    Installation of react with parcel as bundler

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
-//This is the most basic way to create an element with react apis.
-
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
-
-/* 
-
-React element will have 
-1 - a tag
-2 - object which contain attributes
-3 - string to render
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", {}, "h1 tag"),
-  React.createElement("h2", {}, "h2 tag"),
+  React.createElement("h1", {}, "This is react"),
+  React.createElement("h2", {}, "by hrushikesh"),
 ]);
 
 // createroot is an API which was given by ReactDOM.
