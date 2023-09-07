1c54d640dfd2c9f349d677a1b08408626a72c68c
Hrushi <marathehrushikesh930@gmail.com>
2023-09-07 19:43:51 +0530

    Lecture 7 : ‐ Finding the Path

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>about</h1>
+      <h3>react about</h3>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,41 +1,33 @@
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  //useeffect will get called after the rendering is complete.
   useEffect(() => {
     fetchData();
   }, []);
 
-  //creating a function which will fetch data from API.
-  //fetch is an API provided by browser which give apromise.Because of this we have to use async await.we can also use .then.
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.1370256&lng=72.8549697&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://hpmarathe.github.io/swiggy-api/Reslist.json"
     );
 
     const json = await data.json();
     console.log(json);
-    // loads  --> Rendering  --> calling API(after 500 ms will get response) --> Rerender the result from live API.
-    //setListOfRestaurant is responsible for Rerender the result from live API.
     setListOfRestaurants(
       json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
 
-    //here we are setting setFilteredRestaurants same as setListOfRestaurants because in the first cycle we are rendering FilteredRestaurants. so default value should be all restaurants.
     setFilteredRestaurants(
       json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
-  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
-  console.log(" Body rendered");
-  //Using ternary operator
   return listOfRestaurants?.length == 0 ? (
     <Shimmer />
   ) : (
@@ -52,8 +44,6 @@ const Body = () => {
           />
           <button
             onClick={() => {
-              //writing logic for filteredRestaurant - always filtering from all restaurants
-              //using .inclues so if you search for one word it will give results for that word.
               const filteredRestaurant = listOfRestaurants.filter((res) =>
                 res?.info?.name.toLowerCase().includes(searchText.toLowerCase())
               );
@@ -79,7 +69,12 @@ const Body = () => {
       <div className="res-container">
         {/* map over filteredRestaurants variable as first time in the variable both filteredRestaurants & listOfRestaurants are same but if we filtered it for any result then it should render the filtered results*/}
         {filteredRestaurants?.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          <Link
+            key={restaurant?.info?.id}
+            to={"/restaurants/" + restaurant?.info?.id}
+          >
+            <RestaurantCard resData={restaurant} />
+          </Link>
         ))}
       </div>
     </div>
